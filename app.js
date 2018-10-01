8940478a3945e0f193a1ebedf7312dddfcc649c9
Dominique <batistadominique@hotmail.com>
2018-10-01 15:40:18 +0100

    Automated!

--- app.js
+++ app.js
@@ -5,19 +5,38 @@
     my_app.controller('viewerCtrl', [ '$http', '$scope',
         function($http, $scope) {
             $http.get("overlap.json").then(function(res){
-                console.log(res.data);
                 let data = res.data;
                 $scope.network1 = data.network1;
                 $scope.network2 = data.network2;
                 $scope.overlaps = data.overlaps;
-
+                $scope.ignoredKeys = [
+                    "@context",
+                    "@id",
+                    "@type"
+                ];
                 $scope.processed_fields = {};
 
                 for (let schema in data.overlaps){
-                    console.log(data.overlaps[schema])
+                    if (data.overlaps.hasOwnProperty(schema)){
+                        $scope.processed_fields[data.overlaps[schema][0][0]] = [];
+                        $scope.processed_fields[data.overlaps[schema][0][1]] = [];
+
+                        for (let field in data.overlaps[schema][1]["overlapping fields"]){
+                            if (data.overlaps[schema][1]["overlapping fields"].hasOwnProperty(field)){
+                                $scope.processed_fields[data.overlaps[schema][0][0]].push(data.overlaps[schema][1]["overlapping fields"][field][0]);
+                                $scope.processed_fields[data.overlaps[schema][0][1]].push(data.overlaps[schema][1]["overlapping fields"][field][1]);
+                            }
+                        }
+                    }
                 }
             })
         }]
-    )
+    );
+
+    my_app.filter('typeOf', function() {
+        return function (obj) {
+            return typeof obj;
+        };
+    });
 
 })();
